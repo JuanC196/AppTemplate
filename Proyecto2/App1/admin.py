f616a53fdcad0715fb0fc6460ce8906dab1c0b0d
JuanC196 <juanc183.jc@gmail.com>
2023-04-22 14:45:40 -0500

    adicion de super usuario en django y menus intercambiables

--- Proyecto2/App1/admin.py
+++ Proyecto2/App1/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Curso)
+
+admin.site.register(Estudiante)
+
+admin.site.register(Profesor)
+
+admin.site.register(Entregable)
+
+#SuperUsuario
+
+#User: JuanC196
+#Password: superinika1
+
+#Usuario De Prueba
+
+#User: Electra117
+#Password: superinika9
--- Proyecto2/App1/urls.py
+++ Proyecto2/App1/urls.py
@@ -4,10 +4,10 @@ from App1.views import curso
 
 urlpatterns = [
     path('curso/', curso),
-    path('', views.inicio),
+    path('', views.inicio, name="Inicio"),
     path('cursos', views.cursos, name="Cursos"),
-    path('profesores', views.profesores),
-    path('estudiantes', views.estudiantes),
-    path('entregables', views.entregables),
+    path('profesores', views.profesores, name="Profesores"),
+    path('estudiantes', views.estudiantes, name="Estudiantes"),
+    path('entregables', views.entregables, name="Entregables"),
     
 ]
